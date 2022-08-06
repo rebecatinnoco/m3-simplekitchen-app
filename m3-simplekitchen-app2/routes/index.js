2850c81b2463ec6cbd0f62d341c567eaa6351592
rebecatinnoco <104739229+rebecatinnoco@users.noreply.github.com>
2022-08-06 14:14:53 -0400

    add user and pass

---
+++ m3-simplekitchen-app2/routes/index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const auth = require('http-auth');
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+
+const router = express.Router();
+const Registration = mongoose.model('Registration');
+const basic = auth.basic({
+  file: path.join(__dirname, '../users.htpasswd'),
+});
+
+router.get('/', (req, res) => {
+  //res.send('It works!');
+  res.render('index', { title: 'Registration form' });
+});
+router.get('/register', (req, res) => {
+  //res.send('It works!');
+  res.render('register', { title: 'Registration form' });
+});
+
+router.get('/registrations', basic.check((req, res) => {
+  Registration.find()
+    .then((registrations) => {
+      res.render('registrations', { title: 'Listing registrations', registrations });
+    })
+    .catch(() => { 
+      res.send('Sorry! Something went wrong.'); 
+    });
+}));
+
+router.post('/', 
+    [
+        check('name')
+        .isLength({ min: 1 })
+        .withMessage('Please enter a name'),
+        check('email')
+        .isLength({ min: 1 })
+        .withMessage('Please enter an email'),
+        check('username')
+        .isLength({ min: 1 })
+        .withMessage('Please enter a username'),
+        check('password')
+        .isLength({ min: 1 })
+        .withMessage('Please enter a password'),
+    ],
+    async (req, res) => {
+        //console.log(req.body);
+        const errors = validationResult(req);
+        if (errors.isEmpty()) {
+          const registration = new Registration(req.body);
+          //generate salt to hash password
+          const salt = await bcrypt.genSalt(10);
+          //set user to hashed password
+          registration.password = await bcrypt.hash(registration.password, salt);
+          registration.save()
+            .then(() => {res.render('thankyou');})
+            .catch((err) => {
+              console.log(err);
+              res.send('Sorry! Something went wrong.');
+            });
+          } else {
+            res.render('index', { 
+                title: 'Registration form',
+                errors: errors.array(),
+                data: req.body,
+             });
+          }
+    });
+
+module.exports = router;
